06c3818e577e787227914c201b6e26895848d71d
gexiaowei <gandxiaowei@gmail.com>
2019-07-13 23:12:23 +0800

    first init

--- app.js
+++ app.js
@@ -5,34 +5,32 @@ App({
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-
   },
   globalData: {
     userInfo: null,
-    http:"https://www.view-ol.com/viewol_web",
-    web_http:"https://www.view-ol.com/",
+    http: 'https://www.view-ol.com/viewol_web',
+    web_http: 'https://www.view-ol.com/',
     // http: "http://47.93.25.129:8080/viewol_web",
-    encryptedData:null,
-    iv:null,
-    code:null,
-    urlparameter:{
-      "activityType":"-1" , 
-      "keyword":'',
-      "startdate":'',
-      "enddate": '',
-      "catid":'',
-      "isreLaunch":'',
+    encryptedData: null,
+    iv: null,
+    code: null,
+    urlparameter: {
+      'activityType': '-1',
+      'keyword': '',
+      'startdate': '',
+      'enddate': '',
+      'catid': '',
+      'isreLaunch': '',
     },
-    openid:null,
-    uid:null,
-    sessionId:null,
-    isAuthorize:false,
-    url:"",   //来源地址，用于登录完成后跳回到来源页
+    openid: null,
+    uid: null,
+    sessionId: null,
+    isAuthorize: false,
+    url: '',   //来源地址，用于登录完成后跳回到来源页
   },
 
-
   //消息窗口 正方消息窗口
-  t1: function (msg, icon, times=200 , image = "") {//success , loading
+  t1: function (msg, icon, times = 200, image = '') {//success , loading
     wx.showToast({
       title: msg,
       icon: icon,
@@ -57,24 +55,22 @@ App({
     })
   },
 
-
-
-  userLogin: function (urlType="") {
+  userLogin: function (urlType = '') {
     var that = this
 
     //获取encryptedData 和 ivStr 、 级微信用户信息
     wx.getSetting({
       success: res => {
-        console.log("获取授权状态：" + res.authSetting['scope.userInfo'])
+        console.log('获取授权状态：' + res.authSetting['scope.userInfo'])
         if (res.authSetting['scope.userInfo']) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
             success: res => {
-              console.log("================login.js获取信息,写入golobaldata===============")
-              console.log(res);
-              console.log("encryptedData:" + res.encryptedData);
-              console.log("iv:" + res.iv);
-              console.log("================end===============")
+              console.log('================login.js获取信息,写入golobaldata===============')
+              console.log(res)
+              console.log('encryptedData:' + res.encryptedData)
+              console.log('iv:' + res.iv)
+              console.log('================end===============')
               that.globalData.encryptedData = res.encryptedData
               that.globalData.iv = res.iv
 
@@ -84,7 +80,7 @@ App({
               //获取code
               wx.login({
                 success: function (res) {
-                  console.log("登录时强制重新获取code:" + res.code)
+                  console.log('登录时强制重新获取code:' + res.code)
                   var code = res.code
 
                   //通过code、encryptedData、ivStr从服务器换取用户信息
@@ -95,23 +91,22 @@ App({
                       encryptedData: that.globalData.encryptedData,
                       ivStr: that.globalData.iv
                     },
-                    method: "GET",
+                    method: 'GET',
                     dataType: JSON,
-                    header: { 'content-type': 'application/x-www-form-urlencoded' },
+                    header: {'content-type': 'application/x-www-form-urlencoded'},
                     success: function (res) {
                       var re = JSON.parse(res.data)
-                      console.log("======登录======")
-                      console.log("code:" + code)
-                      console.log("encryptedData:" + that.globalData.encryptedData)
-                      console.log("ivStr:" + that.globalData.iv)
+                      console.log('======登录======')
+                      console.log('code:' + code)
+                      console.log('encryptedData:' + that.globalData.encryptedData)
+                      console.log('ivStr:' + that.globalData.iv)
                       console.log(re)
-                      console.log("获取到userId:" + re.result.userId)
-                      console.log("======end======")
-                      
+                      console.log('获取到userId:' + re.result.userId)
+                      console.log('======end======')
 
                       that.globalData.uid = re.result.userId
                       that.globalData.sessionId = re.result.sessionId
-                      
+
                     }
                   })
                 }

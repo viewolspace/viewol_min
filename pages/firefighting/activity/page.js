94794f8b6ba383e974df9171c7dd2ab4b3a35ede
gexiaowei <gandxiaowei@gmail.com>
2019-07-15 05:37:18 +0800

    add 活动接口

--- pages/firefighting/activity/page.js
+++ pages/firefighting/activity/page.js
@@ -1,4 +1,5 @@
-var sliderWidth = 28; // 需要设置slider的宽度，用于计算中间位置
+const { globalData: { http, regeneratorRuntime } } = getApp()
+const sliderWidth = 28; // 需要设置slider的宽度，用于计算中间位置
 
 Page({
     data: {
@@ -21,23 +22,36 @@ Page({
         ],
         activeIndex: 1,
         sliderOffset: 0,
-        sliderLeft: 0
+        sliderLeft: 0,
+        schedule_list: []
     },
-    onLoad: function() {
+    onLoad: async function() {
         var that = this;
-        wx.getSystemInfo({
-            success: function(res) {
-                that.setData({
-                    sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
-                    sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
-                });
-            }
+        await wx.pro.getSystemInfo();
+        that.setData({
+            sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
+            sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
         });
+        this.getScheduleList()
     },
     tabClick: function(e) {
         this.setData({
             sliderOffset: e.currentTarget.offsetLeft,
             activeIndex: e.currentTarget.id
         });
+    },
+
+    getScheduleList: async function() {
+        const { data: { status, result = [], message } } = await wx.pro.request({
+            url: `${http}/schedule/listSchedule`,
+            method: 'GET',
+            data: {
+                expoId: 1,
+                date: '',
+                num: 200
+            }
+        })
+        this.setData({ schedule_list: result })
     }
+
 });

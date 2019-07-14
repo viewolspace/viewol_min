31aa95bcae4f8746e91d115af0e0c3c8d5857250
gexiaowei <gandxiaowei@gmail.com>
2019-07-15 02:52:15 +0800

    完成日历

--- pages/firefighting/activity/page.js
+++ pages/firefighting/activity/page.js
@@ -1,66 +1,43 @@
-// pages/firefighting/index/page.js
+var sliderWidth = 28; // 需要设置slider的宽度，用于计算中间位置
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
-})
+    data: {
+        tabs: [{
+                week: '二',
+                date: 16,
+            },
+            {
+                week: '三',
+                date: 17,
+            },
+            {
+                week: '四',
+                date: 18,
+            },
+            {
+                week: '五',
+                date: 19,
+            },
+        ],
+        activeIndex: 1,
+        sliderOffset: 0,
+        sliderLeft: 0
+    },
+    onLoad: function() {
+        var that = this;
+        wx.getSystemInfo({
+            success: function(res) {
+                that.setData({
+                    sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
+                    sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
+                });
+            }
+        });
+    },
+    tabClick: function(e) {
+        this.setData({
+            sliderOffset: e.currentTarget.offsetLeft,
+            activeIndex: e.currentTarget.id
+        });
+    }
+});

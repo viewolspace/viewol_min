9ccd42aed7d3b735e5251f038db0ee50bab6f266
gexiaowei <gandxiaowei@gmail.com>
2019-07-16 04:08:22 +0800

    增加行业快讯页面

--- pages/firefighting/news/page.js
+++ pages/firefighting/news/page.js
@@ -1,3 +1,33 @@
+const { globalData: { http, regeneratorRuntime } } = getApp()
 Page({
+    data: {
+        imgUrls: [
+            'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+            'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+            'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+        ],
+        lastSeq: '',
+        info_list: [],
+        loadding: false
+    },
+    onLoad: function() {
+        this.getInfoList()
+    },
 
+    getInfoList: async function() {
+        const { lastSeq, info_list } = this
+        const { data: { status, message, result = [] } } = await wx.pro.request({
+            url: `${http}/info/list`,
+            method: 'GET',
+            data: {
+                classify: 1,
+                lastSeq,
+                pageSize: 10
+            }
+        })
+
+        if (status === '0000') {
+            this.setData({ info_list: info_list.contact(result) })
+        }
+    }
 })

c28b408db0ddf9557092211950b8bc1247e9b242
gexiaowei <gandxiaowei@gmail.com>
2019-07-17 03:49:21 +0800

    增加评论点赞

--- pages/firefighting/activity/detail.js
+++ pages/firefighting/activity/detail.js
@@ -1,23 +1,43 @@
 const WxParse = require('../../../wxParse/wxParse.js');
-const { globalData: { http, regeneratorRuntime } } = getApp()
+const { globalData, globalData: { http, regeneratorRuntime } } = getApp()
 
 Page({
     data: {},
-    onLoad: function (option) {
+    onLoad: function(option) {
         const { id } = option
+        this.setData({ id })
         this.getActivityInformation(id)
     },
 
-    getActivityInformation: async function (id) {
+    getActivityInformation: async function(id) {
         const { data: { status, message, result } } = await wx.pro.request({
             url: `${http}/schedule/getSchedule`,
             method: 'GET',
-            data: { id }
+            data: { id, userId: globalData.uid }
         })
 
-        if (status === '0000'){
-            this.setData({ info: result })
+        if (status === '0000') {
+            this.setData({ info: result, applyStatus: +result.applyStatus })
             WxParse.wxParse('article', 'html', result.contentView, this, 5);
-        } 
+        }
+    },
+    applyJoin: async function() {
+        const { id } = this.data
+        const { data: { status, message } } = await wx.pro.request({
+            url: `${http}/schedule/applyJoin`,
+            method: 'POST',
+            data: { scheduleId: id, userId: globalData.uid, needReminder: false },
+            header: { 'content-type': 'application/x-www-form-urlencoded' }
+        })
+
+        wx.showToast({
+            title: message,
+            icon: status === '0000' ? 'success' : 'none',
+            duration: 3000
+        });
+
+        if (status === '0000') {
+            this.setData({ applyStatus: 1 })
+        }
     }
 })
--- pages/firefighting/products/detail.js
+++ pages/firefighting/products/detail.js
@@ -1,22 +1,25 @@
 const WxParse = require('../../../wxParse/wxParse.js');
-const { globalData: { http, expoId, regeneratorRuntime } } = getApp()
+const { globalData, globalData: { http, expoId, regeneratorRuntime } } = getApp()
 
 Page({
     data: {
-        id: null
+        id: null,
+        message: ''
     },
     onLoad: function(option) {
-        this.getProductInformation(option.id)
+        this.setData({ id: option.id })
+        this.getProductInformation()
     },
 
-    getProductInformation: async function(id) {
+    getProductInformation: async function() {
+        const { id } = this.data
         const { data: { code, collection, comment, company, isPraise, message, praise, result: product, see } } = await wx.pro.request({
             url: `${http}/product/getProduct`,
             method: 'GET',
             data: { id }
         })
         if (code === '0000') {
-            this.setData({ code, collection, comment, company, isPraise, message, praise, product, see })
+            this.setData({ code, collection, comment, company, isPraise, praise, product, see })
             WxParse.wxParse('article', 'html', product.contentView, this, 5);
             this.getRelativeProduct(company.id)
         }
@@ -38,6 +41,55 @@ Page({
                 relative_products: result
             })
         }
+    },
+
+    praiseProduct: async function() {
+        const { isPraise, id } = this.data
+        if (isPraise !== 0) return
+
+        const { data: { status } } = await wx.pro.request({
+            url: `${http}/product/praise`,
+            method: 'POST',
+            data: {
+                productId: id,
+                userId: globalData.uid
+            },
+            header: { 'content-type': 'application/x-www-form-urlencoded' }
+        })
+
+        if (status === '0000') {
+            wx.showToast({
+                title: '点赞成功',
+                icon: 'success',
+                duration: 3000
+            });
+        }
+    },
+
+    commitComment: async function(event) {
+        const { id } = this.data
+        const { value: content } = event.detail
+        const { data: { status } } = await wx.pro.request({
+            url: `${http}/product/comment`,
+            method: 'POST',
+            data: {
+                productId: id,
+                userId: globalData.uid,
+                content
+            },
+            header: { 'content-type': 'application/x-www-form-urlencoded' }
+        })
+
+        if (status === '0000') {
+            wx.showToast({
+                title: '评论成功',
+                icon: 'success',
+                duration: 3000
+            });
+            this.setData({ message: '' })
+            this.getProductInformation()
+        }
+
     }
 
 })
